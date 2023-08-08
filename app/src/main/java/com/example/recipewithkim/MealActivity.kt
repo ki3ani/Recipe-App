1886d3a67859afbdd132297763a333c0c0038c48
ki3ani <kimanim68@gmail.com>
2023-08-08 20:34:12 +0300

    adding-you-tube

--- app/src/main/java/com/example/recipewithkim/MealActivity.kt
+++ app/src/main/java/com/example/recipewithkim/MealActivity.kt
@@ -1,7 +1,10 @@
 package com.example.recipewithkim
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
@@ -14,6 +17,7 @@ class MealActivity : AppCompatActivity() {
     private lateinit var mealName:String
     private lateinit var mealThumb:String
     private lateinit var binding:ActivityMealBinding
+    private lateinit var youtubelink:String
     private lateinit var mealMvvm:MealViewModel
 
 
@@ -28,18 +32,31 @@ class MealActivity : AppCompatActivity() {
 
         setInformationInViews()
 
+        loadingCase()
+
         mealMvvm.getMealDetail(mealId)
         observerMealDetailsLiveData()
+
+        onYoutubeImageClick()
+    }
+
+    private fun onYoutubeImageClick() {
+        binding.imgYoutube.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubelink) )
+            startActivity(intent)
+        }
     }
 
     private fun observerMealDetailsLiveData() {
         mealMvvm.observerMealDetailsLiveData().observe(this,object : Observer<Meal>{
             override fun onChanged(value: Meal) {
+                onResponseCase()
                 val meal = value
 
                 binding.tvCategory.text = "Category : ${meal!!.strCategory}"
                 binding.tvOrigin.text = "Origin : ${meal.strArea}"
                 binding.tvInstructionsSteps.text = meal.strInstructions
+                youtubelink = meal.strYoutube
             }
 
         })
@@ -62,4 +79,26 @@ class MealActivity : AppCompatActivity() {
         mealThumb = intent.getStringExtra(HomeFragment.MEAL_THUMB)!!
 
     }
+
+    private fun loadingCase(){
+        binding.progressBar.visibility =View.VISIBLE
+        binding.btnAddToFav.visibility = View.INVISIBLE
+        binding.tvInstructions.visibility = View.INVISIBLE
+        binding.tvCategory.visibility = View.INVISIBLE
+        binding.tvOrigin.visibility = View.INVISIBLE
+        binding.imgYoutube.visibility = View.INVISIBLE
+
+    }
+
+    private fun onResponseCase(){
+        binding.progressBar.visibility =View.INVISIBLE
+        binding.btnAddToFav.visibility = View.VISIBLE
+        binding.tvInstructions.visibility = View.VISIBLE
+        binding.tvCategory.visibility = View.VISIBLE
+        binding.tvOrigin.visibility = View.VISIBLE
+        binding.imgYoutube.visibility = View.VISIBLE
+
+
+    }
 }
+
