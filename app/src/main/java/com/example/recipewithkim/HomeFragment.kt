add7f51952a7065280e3cf9c278e1499743dcce6
ki3ani <kimanim68@gmail.com>
2023-08-08 15:30:38 +0300

    randomeal

--- app/src/main/java/com/example/recipewithkim/HomeFragment.kt
+++ app/src/main/java/com/example/recipewithkim/HomeFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.recipewithkim.databinding.FragmentHomeBinding
 import com.example.recipewithkim.pojo.Meal
 import com.example.recipewithkim.pojo.MealList
 import com.example.recipewithkim.retrofit.RetroFitInstance
@@ -14,6 +16,10 @@ import retrofit2.Response
 import javax.security.auth.callback.Callback
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,8 +31,8 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -36,7 +42,9 @@ class HomeFragment : Fragment() {
             override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
                 if(response.body() != null){
                     val randomMeal: Meal = response.body()!!.meals[0]
-                    Log.d("TEST","meal id ${randomMeal.idMeal} name ${randomMeal.strMeal}")
+                    Glide.with(this@HomeFragment)
+                        .load(randomMeal.strMealThumb)
+                        .into(binding.imgRandomMeal)
                 }else{
                     return
                 }
