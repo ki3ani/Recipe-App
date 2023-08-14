0cec7c7c180597c45833aa24f6a2160262818239
ki3ani <kimanim68@gmail.com>
2023-08-15 00:24:43 +0300

    adding-favorites-page

--- app/src/main/java/com/example/recipewithkim/FavoritesFragment.kt
+++ app/src/main/java/com/example/recipewithkim/FavoritesFragment.kt
@@ -7,12 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.recipewithkim.adapters.FavoriteMealsAdapter
 import com.example.recipewithkim.databinding.FragmentFavoritesBinding
 import com.example.recipewithkim.viewModel.HomeViewModel
 
 class FavoritesFragment : Fragment() {
     private lateinit var binding:FragmentFavoritesBinding
     private lateinit var viewModel:HomeViewModel
+    private lateinit var favoritesAdapter:FavoriteMealsAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,13 +37,22 @@ class FavoritesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         observeFavorites()
+        prepareRecylerView()
+    }
+
+    private fun prepareRecylerView() {
+        favoritesAdapter = FavoriteMealsAdapter()
+        binding.rvFavorites.apply {
+            layoutManager = GridLayoutManager(context,2,GridLayoutManager.VERTICAL,false)
+            adapter = favoritesAdapter
+        }
+
     }
 
     private fun observeFavorites() {
         viewModel.observeFavoritesMealsLiveData().observe(viewLifecycleOwner, Observer { meals->
-            meals.forEach {
-                Log.d("test",it.idMeal)
+            favoritesAdapter.differ.submitList(meals)
             }
-        })
-    }
+        )}
+
 }
